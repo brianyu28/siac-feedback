2b15504f71d6713ead2c19eaebf58ee5a36a6d21
Brian Yu <brianyu28@gmail.com>
2016-07-06 02:08:53 -0400

    Added under construction message

--- siac.py
+++ siac.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "This is SIAC Feedback."
+    return "This is SIAC Feedback. This website is currently under construction."
 
 if __name__ == "__main__":
     app.run()
